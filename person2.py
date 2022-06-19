9e011ff47e5a4ab9eb753ccf74c8ec81108de57a
ratchakoonprn <ratchakoon.prn@outlook.com>
2022-06-19 09:36:03 +0700

    modify person2

--- person2.py
+++ person2.py
@@ -11,17 +11,20 @@ class Person:
     def show(self):
         print(f'Name: {self.name} Gender: {self.gender} Profession: {self.profession} Study: {self.study}')
 
-#person1
-jessa = Person('Jessa','Female','Software Engineer',10)
-jessa.work()
-jessa.show()
+    
 
-#person2
-jon = Person('Jon','Male','Doctor',15)
-jon.work()
-jon.show()
+if __name__ =="__main__":
+    #person1
+    jessa = Person('Jessa','Female','Software Engineer',10)
+    jessa.work()
+    jessa.show()
 
-#person
-lisa = Person('Lisa','Female','Singer',10)
-lisa.work()
-lisa.show()
+    #person2
+    jon = Person('Jon','Male','Doctor',15)
+    jon.work()
+    jon.show()
+
+    #person
+    lisa = Person('Lisa','Female','Singer',10)
+    lisa.work()
+    lisa.show()
