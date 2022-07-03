aeea4ccf9e436f8185c6b7075f99fc33adab5af7
ratchakoonprn <ratchakoon.prn@outlook.com>
2022-07-03 13:55:54 +0700

    mark non property

--- encapsulation/getter_setter/objarea.py
+++ encapsulation/getter_setter/objarea.py
@@ -4,3 +4,10 @@ area = Area()
 area.base = float(input("Enter base: "))
 area.high = float(input("Enter high: "))
 print(f'Area = {area.compute_area():.2f}')
+
+"""#ไม่ใช่ property
+base = float(input("Enter base: "))
+high = float(input("Enter high: "))
+area.setbase(base)
+area.sethigh(high)
+area.compute_area()"""
