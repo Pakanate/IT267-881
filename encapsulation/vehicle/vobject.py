34a87566672e23464cb04f67423a7a2725e7cf2b
ratchakoonprn <ratchakoon.prn@outlook.com>
2022-07-03 13:17:30 +0700

    vehicle object

--- encapsulation/vehicle/vobject.py
+++ encapsulation/vehicle/vobject.py
@@ -5,3 +5,10 @@ b1 = Bus("Volvo",4,120,"v123")
 b1.set_color("Blue")
 b1.set_capacity(34)
 b1.v_detail()
+
+#object motocycle
+from motocycle import Motocycle
+
+m1 = Motocycle("Honda",2,100,"V345")
+m1.set_cc(1200)
+m1.v_detail()
